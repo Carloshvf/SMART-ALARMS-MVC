43ce31601d692491295732561f6cf17a7ab1a597
Gustavo <gustavo.sa@spassu.com>
2019-12-18 17:43:36 -0300

    home, routing links e criacao detalhes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PainelCentralComponent } from '../app/painel-central/painel-central.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'crisis-center', component: PainelCentralComponent },
-  { path: '**', component: PainelCentralComponent }
+  { path: 'painel-central', component: PainelCentralComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PainelCentralComponent } from './painel-central/painel-central.component';
+import { HomeComponent } from './home/home.component';
+import { DetalhesComponent } from './detalhes/detalhes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PainelCentralComponent
+    PainelCentralComponent,
+    HomeComponent,
+    DetalhesComponent
   ],
   imports: [
     BrowserModule,
