d900552494361ab60344fac28832d59140071751
Gustavo <gustavo.sa@spassu.com>
2019-12-19 12:56:18 -0300

    componente detalhes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PainelCentralComponent } from '../app/painel-central/painel-central.component';
 import { HomeComponent } from './home/home.component';
+import { DetalhesComponent } from '../app/detalhes/detalhes.component';
 
 const routes: Routes = [
   { path: 'painel-central', component: PainelCentralComponent },
+  { path: 'detalhes', component: DetalhesComponent },
   { path: '**', component: HomeComponent }
 ];
 
