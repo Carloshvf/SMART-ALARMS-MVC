40be76e63876ef81373e2676cf2f8f147c613300
carlos.fernandes <carlos.fernandes@spassu.com.br>
2020-04-28 10:29:07 -0300

    Subindo select alterado em Canais

--- static/sw.js
+++ static/sw.js
@@ -6,28 +6,20 @@ workbox.precaching.precacheAndRoute([
     "revision": "db26aa1e9954d515231b3b3658905c8c"
   },
   {
-    "url": "/_nuxt/2aabd87db9ba03029864.js",
-    "revision": "a0dc611ec82a693d3dc82518bdcaf3c0"
-  },
-  {
     "url": "/_nuxt/4aca815edaf2576bc91d.js",
     "revision": "4550b8ac7ca99f8f47193aee4c7ab9d8"
-  },
-  {
-    "url": "/_nuxt/6570caf60dd38591d855.js",
-    "revision": "10bb2a5c47c1c9b86f0bcdd0eae4b3e9"
   },
   {
     "url": "/_nuxt/8f9b4f0f3d95451ec447.js",
     "revision": "00d1a83892f9f3f3128cea2331e1e8ad"
   },
   {
-    "url": "/_nuxt/a12dcc3a24612745e8d3.js",
-    "revision": "0c8c0031d3271ca0ce004cee3e6cd2be"
+    "url": "/_nuxt/a3aa78662459cbc22943.js",
+    "revision": "9171bd4cca5396a21b9d9e38362a0f0f"
   },
   {
-    "url": "/_nuxt/a47e69c1c48c92a109d3.js",
-    "revision": "ffeb0c47c78859d84cdd41dbfea072dd"
+    "url": "/_nuxt/a8b3f00743bc7c04515c.js",
+    "revision": "381e451b8bfd7f8d7fd37fcc3d610ae0"
   },
   {
     "url": "/_nuxt/a95a7ee4e05662273ece.js",
@@ -38,12 +30,20 @@ workbox.precaching.precacheAndRoute([
     "revision": "5edca0b1ce51f70d7fc08c6daa5648ee"
   },
   {
+    "url": "/_nuxt/d1d7a6c533947c2c8f21.js",
+    "revision": "ca9534c34315221e8c39877a7818d7de"
+  },
+  {
     "url": "/_nuxt/d6aa818445bce8057587.js",
     "revision": "8e8c66dde8abd53f9b7c5d5cde5cbf53"
   },
   {
     "url": "/_nuxt/e255a50c973994bca09c.js",
     "revision": "046ad5ad8e0b19c5038559f6c0f4207d"
+  },
+  {
+    "url": "/_nuxt/e2610aa54955df398245.js",
+    "revision": "298f28350533482015890802f4e28a88"
   },
   {
     "url": "/_nuxt/fb71ab601c2623e28ebc.js",
