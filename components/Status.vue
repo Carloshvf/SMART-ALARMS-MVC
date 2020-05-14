a814ab1ae4d9c7f90f9562d453c5d69527f6f3dc
carlos.fernandes <carlos.fernandes@spassu.com.br>
2020-05-14 10:42:38 -0300

    Corrigindo bug dos graficos das medidas

--- components/Status.vue
+++ components/Status.vue
@@ -15,10 +15,12 @@
               <graph ref="chartCurve" :chart-data="fillData" :height="210" :options="chartOptions" />
               <b-button @click="reset">Reset zoom</b-button>
             </b-modal>
-            <h6 class="mb-3 text-uppercase p-2 bg-light-purple" v-b-modal="equip.kks" @click="getGraph(equip.kks)" :disabled="disable" v-if="equip.tipo == 'medida'">
-              {{ equip.kks }} - {{ equip.valor_medida }}
-            </h6>
-            <h6 class="mb-3 text-uppercase p-2" v-if="equip.tipo == 'status'" :class="{ red: equip.color == 'red', green: equip.color == 'green' }">{{ equip.kks }}</h6>
+            <b-button class="mb-3 text-uppercase p-2 bg-light-purple btn-block" v-b-modal="equip.kks" @click="getGraph(equip.kks)" :disabled="disable" v-if="equip.tipo == 'medida'">
+              <b>{{ equip.kks }} - {{ equip.valor_medida }} </b>
+            </b-button>
+            <b-button class="mb-3 text-uppercase p-2 btn-block" v-if="equip.tipo == 'status'" :class="{ red: equip.color == 'red', green: equip.color == 'green' }">
+              <b>{{ equip.kks }}</b>
+            </b-button>
             <div class="card-equip-cause bg-light-purple p-2" :class="{ dNone: equip.isHidden }">
               <small class="text-uppercase">causa</small>
               <p>{{ equip.cause }}</p>
@@ -159,13 +161,13 @@ export default {
           clearInterval(this.rerun)
          
         }
-      }, 15000);
+      }, 5000);
       }
       
       this.disable = true
       setTimeout(() => {
        this.disable = false
-     }, 15000);
+     }, 5000);
     },
 
     async getGraph(id) {
@@ -186,7 +188,7 @@ export default {
           clearInterval(this.stopInterval)
          
         }
-      }, 13000);
+      }, 3000);
       this.ende = id
 
       this.chartOptions.scales.yAxes[0].scaleLabel.labelString = this.fillData.value
--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -139,7 +139,7 @@ export default {
           clearInterval(this.stopInterval)
          
         }
-      }, 15000);
+      }, 5000);
    
   },
 }
