cca5a06ed4e631f48c0742ae1b5483f30f55eeac
Gabriel Lima <gabriel.lima@spassu.com>
2020-05-19 11:18:34 -0300

    configurando para rodar na petrobras

--- dist/sw.js
+++ dist/sw.js
@@ -6,6 +6,10 @@ workbox.precaching.precacheAndRoute([
     "revision": "da32bd844993ca38ef3f85a16294bdd0"
   },
   {
+    "url": "/_nuxt/2aabd87db9ba03029864.js",
+    "revision": "a0dc611ec82a693d3dc82518bdcaf3c0"
+  },
+  {
     "url": "/_nuxt/67a1e49b534ac36d8f4a.js",
     "revision": "93783fc1757df26de7c9b6fa9fbb94fd"
   },
@@ -40,10 +44,6 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/d6aa818445bce8057587.js",
     "revision": "8e8c66dde8abd53f9b7c5d5cde5cbf53"
-  },
-  {
-    "url": "/_nuxt/e2610aa54955df398245.js",
-    "revision": "298f28350533482015890802f4e28a88"
   },
   {
     "url": "/_nuxt/fb71ab601c2623e28ebc.js",
