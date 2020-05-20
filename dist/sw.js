5a7cdf0a736a9ba37c914f5fba79a42ce43144b7
Gabriel Lima <gabriel.lima@spassu.com>
2020-05-20 17:08:05 -0300

    gerando dist

--- dist/sw.js
+++ dist/sw.js
@@ -6,6 +6,10 @@ workbox.precaching.precacheAndRoute([
     "revision": "c2b7fe8bf28c940931f6ab2c2c292200"
   },
   {
+    "url": "/_nuxt/502268cb149f9100eb13.js",
+    "revision": "4d30fc5dce592b26081a805c5d43f642"
+  },
+  {
     "url": "/_nuxt/66298351715d5c73134b.js",
     "revision": "498d20017c43d82f57e9017c428e3eb5"
   },
@@ -14,8 +18,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "93783fc1757df26de7c9b6fa9fbb94fd"
   },
   {
-    "url": "/_nuxt/84e23b46004c3e023cdb.js",
-    "revision": "d79a8b6c9f53f45ee5f7abd8011f84b2"
+    "url": "/_nuxt/6860fdbe3aafe3f9385d.js",
+    "revision": "4b5043ad0d4ebd941ae78f958808bd05"
+  },
+  {
+    "url": "/_nuxt/6b96d23e6f2ca3ea7172.js",
+    "revision": "f0368372278c4b04069b9ef4059dad5e"
   },
   {
     "url": "/_nuxt/a3aa78662459cbc22943.js",
@@ -26,20 +34,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "381e451b8bfd7f8d7fd37fcc3d610ae0"
   },
   {
-    "url": "/_nuxt/b6e3743429008e732afa.js",
-    "revision": "64b0418150d25280f38da42bc26d0945"
-  },
-  {
     "url": "/_nuxt/bf8e24a7e7aecde3f2f8.js",
     "revision": "312f32cb05465d42355130bb874d73d3"
   },
   {
     "url": "/_nuxt/c005bcab6ffd878d0833.js",
     "revision": "5edca0b1ce51f70d7fc08c6daa5648ee"
-  },
-  {
-    "url": "/_nuxt/c1adadbec94914ce2ef0.js",
-    "revision": "f58c87ebd269790e90b21bdcc038790f"
   },
   {
     "url": "/_nuxt/d6aa818445bce8057587.js",
