f2eefd24af18fdd52cf35e05848f2430b82d429d
carlos.fernandes <carlos.fernandes@spassu.com.br>
2020-06-10 12:21:23 -0300

    Alterações em layout e correção de erros

--- pages/alarm/index.vue
+++ pages/alarm/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="py-5 mt-5">
     <div
-      class="wrapper-content container"
+      class="wrapper-content container-fluid"
       v-for="list in lists"
       :key="list.name"
     >
-      <div class="row card mb-4" v-for="card in list.kks" :key="card.id">
+      <div class="row card card-avalanche mb-4" v-for="card in list.kks" :key="card.id">
         <div class="col-12 card-kks" v-if="list.active == 1">
             <div class="row">
               <div class="col-2 mt-3">
@@ -70,6 +70,7 @@ export default {
   data() {
     return {
       // lists: this.$store.state.all
+      arrSize: [],
       countTime: '',
       stop: true,
       stopInterval: ""
@@ -121,6 +122,17 @@ export default {
       this.stopInterval = setInterval(() => {
         if (this.stop == true) {
           this.loadData()
+          this.arrSize.splice(0)
+
+          for (let index = 0; index < this.lists.length; index++) {
+          if (this.lists[index].active == 0 && this.currentRouteName == 'alarm') {
+              this.arrSize.push("5") 
+            }
+          }
+          if (this.arrSize.length == this.lists.length && this.currentRouteName == 'alarm') {
+            this.$router.push('/')
+          }
+
           if (this.currentRouteName != 'alarm') {
             this.stop = false 
           }
@@ -136,7 +148,8 @@ export default {
 @import '~/assets/scss/base.scss';
 
 .card-avalanche {
-  width: 300px;
+  width: 80%;
+  margin: 0 auto;
 }
 
 .shadow {
