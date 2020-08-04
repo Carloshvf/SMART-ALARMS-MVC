fd485163220f112a5bac1268b5a9f6a82ca32f7b
carlos.fernandes <carlos.fernandes@spassu.com.br>
2020-08-04 12:51:16 -0300

    Ajustando a estrutura html da página de sugestões

---
+++ components/SuggestionDetail.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="row card-suggest">
+        <div class="col suggest-options">
+            <div class="row mt-2">
+                <div class="col-1">
+                    <img src="../static/img/pencil-box.svg" alt="Edit" />   
+                </div>
+                <div class="col-1" style="color:#02592E;">31</div>
+                <div class="col-1" style="color:#02592E;">PLST</div>
+                <div class="col-5">Elevação de temperatura da turbina axial</div>
+                <div class="col-4 status-align">
+                    <span class="badge green mr-1">&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    Implementada
+                </div>
+                <div class="row card-suggest">
+                    <div class="col-11 mt-1">
+                        <span>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                            sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </span>
+                    </div>
+                    <b-button class="btn btn-consulta" v-b-modal="'my-modal'">CONSULTA</b-button>
+                </div>
+                
+                <div class="col-3 mb-3">
+                    <span style="color: #B5B5B5;">Nome: Diego Machado</span>
+                </div>
+                <div class="col-4 mb-3">
+                    <span style="color: #B5B5B5;">Chave de acesso: 123456789</span>
+                </div>
+                <div class="col-4 mb-3">
+                    <span style="color: #B5B5B5;">Data e hora: 16/06/2020 às 12:00</span>
+                </div>
+            </div>
+            <!-- MODAL -->
+            <b-modal size="xl" id="my-modal">Hello From My Modal!</b-modal> 
+            <!--  -->
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/base.scss';
+
+.suggest-options {
+    background: #FFFFFF;
+    border: 1px solid #999999;
+    border-radius: 4px;
+}
+
+.btn-consulta {
+    text-align: center;
+    position: absolute;
+    right: 25px;
+    color: $white;
+    background: #6979F8;
+    border-radius: 5px;
+
+    &:hover{
+        color: $white;
+        background-color: #6979F8;
+    } 
+}
+
+</style>
--- pages/suggestion/index.vue
+++ pages/suggestion/index.vue
@@ -20,63 +20,24 @@
                             <div class="col-1">UG</div>
                             <div class="col-1">TIPO</div>
                             <div class="col-3">CAUSA BÁSICA</div>
-                            <div class="col-6 status-align">STATUS</div>
+                            <div class="col-4 status-align">STATUS</div>
                         </div>
                     </div>
                 </div>
-                <div class="row card-suggest">
-                    <div class="col suggest-options">
-                        <div class="row mt-2">
-                            <div class="col-1">
-                                <img src="../../static/img/pencil-box.svg" alt="Edit" />   
-                            </div>
-                            <div class="col-1" style="color:#02592E;">31</div>
-                            <div class="col-1" style="color:#02592E;">PLST</div>
-                            <div class="col-3">Elevação de temperatura da turbina axial</div>
-                            <div class="col-6 status-align">
-                                <span class="badge green mr-1">&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                                Implementada
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-11 mt-3 mb-2">
-                                <span>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                                    sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                </span>
-                            </div>
-                            <div class="col-1">
-                                <b-button class="btn btn-consulta" v-b-modal="'my-modal'">CONSULTA</b-button>
-                            </div>
-                            <div class="col-3 mt-2 mb-3">
-                                <span style="color: #B5B5B5;">Nome: Diego Machado</span>
-                            </div>
-                             <div class="col-4 mt-2 mb-3">
-                                <span style="color: #B5B5B5;">Chave de acesso: 123456789</span>
-                            </div>
-                             <div class="col-4 mt-2 mb-3">
-                                <span style="color: #B5B5B5;">Data e hora: 16/06/2020 às 12:00</span>
-                            </div>
-                        </div>
-                        <!-- MODAL -->
-                        <b-modal size="xl" id="my-modal">Hello From My Modal!</b-modal> 
-                        <!--  -->
-                    </div>
-                </div>
+                <suggestion-detail/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import SuggestionCard from '~/components/SuggestionCard.vue'
+import SuggestionCard from '~/components/SuggestionCard.vue';
+import SuggestionDetail from '~/components/SuggestionDetail.vue';
 
 export default {
     components: {
-        SuggestionCard
+        SuggestionCard,
+        SuggestionDetail
     },
 
     methods: {
@@ -109,6 +70,11 @@ export default {
 .card-size {
     width: 70%;
     margin: 0 auto;
+
+    h1 {
+        position: relative;
+        right: 18px;
+    }
 }
 .status-align {
     position: absolute;
@@ -121,12 +87,6 @@ export default {
     border-radius: 4px;
 }
 
-.suggest-options {
-    background: #FFFFFF;
-    border: 1px solid #999999;
-    border-radius: 4px;
-}
-
 .cross-img {
     position: absolute;
     right: 73.38%;
@@ -135,20 +95,6 @@ export default {
 
 .green {
     background: #6DD400;
-}
-
-.btn-consulta {
-    position: absolute;
-    right: 25px;
-    color: $white;
-    background: #6979F8;
-    border-radius: 5px;
-
-    &:hover{
-        color: $white;
-        background-color: #6979F8;
-    }
-    
 }
 
 </style>
