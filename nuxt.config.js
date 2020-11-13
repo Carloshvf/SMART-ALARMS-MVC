21434216dea8f245a58e416552503ca8aeb20417
carlos.fernandes <carlos.fernandes@spassu.com.br>
2020-11-13 12:33:10 -0300

    Voltando o nuxt pra universal

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     baseURL: process.env.baseURL || 'https://api-smartalarms-dev.transformacaodigitalspassu.com.br:3000'
   },
 
-  mode: 'spa',
+  mode: 'universal',
   /*
    ** Headers of the page
    */
