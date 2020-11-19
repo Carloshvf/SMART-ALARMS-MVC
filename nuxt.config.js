a2c959cc085a079a3f5066b34b8b037c69f266d3
carlos.fernandes <carlos.fernandes@spassu.com.br>
2020-11-19 17:25:24 -0300

    teste de plugin server side

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{src: '~/plugins/main.js.server.js'}],
+  plugins: [{src: '~/plugins/main.js', mode: 'server'}],
   /*
    ** Nuxt.js modules
    */
