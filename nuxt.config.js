347fdcf2a4ee3496028b7798bf69c8cfa6cf0896
carlos.fernandes <carlos.fernandes@spassu.com.br>
2020-11-23 14:40:29 -0300

    Tentando ajeitar plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/main.js'],
+  plugins: [{src:'~/plugins/main.client.js'}],
   /*
    ** Nuxt.js modules
    */
