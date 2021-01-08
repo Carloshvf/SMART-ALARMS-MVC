87d5f35533d2a9cccfcddadde18b6e349a23cbeb
carlos.fernandes <carlos.fernandes@spassu.com.br>
2021-01-08 14:12:28 -0300

    preparo para build

--- dist/sw.js
+++ dist/sw.js
@@ -10,10 +10,6 @@ workbox.precaching.precacheAndRoute([
     "revision": "681b14bc69120a1805d029c6a3fee1bd"
   },
   {
-    "url": "/_nuxt/1618ce7ccaf93c78048a.js",
-    "revision": "e4cff51bf2f8f09c48d09a70e0967dd4"
-  },
-  {
     "url": "/_nuxt/3f6956d24e4338d33d58.js",
     "revision": "e5c0ec57612bd288e373a2f2019662f3"
   },
@@ -38,6 +34,10 @@ workbox.precaching.precacheAndRoute([
     "revision": "140b71bafe4cbe912e94493cee7508bb"
   },
   {
+    "url": "/_nuxt/54d32d990f94503d2871.js",
+    "revision": "fda721ba5c0f7f09562c703032ffdbb9"
+  },
+  {
     "url": "/_nuxt/68c00798c8dba596afd5.js",
     "revision": "7a5bde4033961567c78fa02616ea33bf"
   },
@@ -54,10 +54,6 @@ workbox.precaching.precacheAndRoute([
     "revision": "9f911c5ba0de76d321da8568980c6734"
   },
   {
-    "url": "/_nuxt/b1516096029ec7c97322.js",
-    "revision": "8eef1e0b3c2044f0e47fdc7e989bbf5c"
-  },
-  {
     "url": "/_nuxt/b74353785d709e68273c.js",
     "revision": "8f276eb4fef63e7073386b76b569ad45"
   },
@@ -68,6 +64,10 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/e67b37c12b4e56403707.js",
     "revision": "20d250643a5d5c51a43aedf7b0c339ac"
+  },
+  {
+    "url": "/_nuxt/f8096bee43db45fd64c9.js",
+    "revision": "2e68b23a25b1de2af87a9396f1be8e0f"
   },
   {
     "url": "/_nuxt/f97b8f04e89ef4509194.js",
