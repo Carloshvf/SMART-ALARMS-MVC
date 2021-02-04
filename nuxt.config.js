7edd506b38055bc2c20e8723b54c031e490b3f38
carlos.fernandes <carlos.fernandes@spassu.com.br>
2021-02-04 11:38:48 -0300

    tentando preparar para build

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ export default {
   env: {
     //PEGANDO A VARIAVEL DE AMBIENTE OU PEGAR A STRING MOCADA
     baseURL: process.env.baseURL || 'https://smartalarms-backend-hmg.petrobras.com.br'
+
   },
 
   mode: 'spa',
