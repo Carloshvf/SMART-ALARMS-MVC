f64ae7aaf613459f7158e2e92f7cfac4c133f5df
onezer00 <caimbebr@gmail.com>
2021-10-06 16:18:30 -0300

    Smartalarms: re-build.

--- static/sw.js
+++ static/sw.js
@@ -12,5 +12,4 @@ workbox.clientsClaim();
 workbox.skipWaiting();
 
 workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET');
-
 workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET');
