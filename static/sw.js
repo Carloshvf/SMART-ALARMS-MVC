c5ff7a3167ac0c7d8bf218e713170c0b2a6bb23d
carlos.fernandes <carlos.fernandes@spassu.com.br>
2021-10-15 18:49:55 -0300

    mundaças

--- static/sw.js
+++ static/sw.js
@@ -13,3 +13,4 @@ workbox.skipWaiting();
 
 workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET');
 workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET');
+
