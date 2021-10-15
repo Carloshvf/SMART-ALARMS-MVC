2afebe4c426c81979c3d33278678341cbf9e910c
carlos.fernandes <carlos.fernandes@spassu.com.br>
2021-10-15 18:56:32 -0300

    alterações

--- static/sw.js
+++ static/sw.js
@@ -13,4 +13,3 @@ workbox.skipWaiting();
 
 workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET');
 workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET');
-
