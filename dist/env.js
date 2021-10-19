c971bfc3bbafe2ad52d3349e48e2170f633bf4b0
bruno.garcia <bruno.garcia@spassu.com.br>
2021-10-19 14:23:06 -0300

    atualizado os arquivos do dist !

---
+++ dist/env.js
@@ -0,0 +1,19 @@
+const AWS_URL = "https://api-smartalarms-dev.transformacaodigitalspassu.com.br:3000"
+var ProcessEnv = {
+    BASE_URL:AWS_URL
+}
+
+window.processEnv = ProcessEnv;
+
+var HOST = window.location.host;
+
+if(HOST === 'localhost:3000' || HOST === 'localhost:3100') {
+    window.processEnv.BASE_URL = AWS_URL;
+} else if(HOST === 'smartalarms-dev.transformacaodigitalspassu.com.br:3100') {
+    window.processEnv.BASE_URL = AWS_URL;
+} else if (HOST === 'smartalarms-hmg.petrobras.com.br') {
+    window.processEnv.BASE_URL = 'https://smartalarms-backend-hmg.petrobras.com.br';
+} else {
+    window.processEnv.BASE_URL = 'https://smartalarms-backend.petrobras.com.br';
+}
+
